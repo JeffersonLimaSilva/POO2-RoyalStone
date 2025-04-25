dee0eff567733bacfd4d74b481fbc9a44a36e503
Silvio Henrique <96311008+silvioGPS@users.noreply.github.com>
2025-04-24 22:30:35 -0300

    Spring-Boot

---
+++ royalstone/src/main/java/br/com/royalstone/RoyalstoneApplication.java
@@ -0,0 +1,13 @@
+package br.com.royalstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalstoneApplication.class, args);
+	}
+
+}
---
+++ royalstone/src/main/java/br/com/royalstone/model/Origem.java
@@ -0,0 +1,26 @@
+package br.com.royalstone.model;
+
+public class Origem {
+	private String pais;
+	private String cidade;
+	private String tipoExtracao;
+	
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getTipoExtracao() {
+		return tipoExtracao;
+	}
+	public void setTipoExtracao(String tipoExtracao) {
+		this.tipoExtracao = tipoExtracao;
+	}
+}
